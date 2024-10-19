0c2506ab934058d7596cdb8b2eb18278cb77a7d4
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2024-10-19 10:55:23 +0200

    c ue3

---
+++ C/UE3/lib.c
@@ -0,0 +1,11 @@
+# include "lib.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
---
+++ C/UE3/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_H
+#define LIB_H
+
+int add(int a, int b);
+int sub(int a, int b);
+#endif
